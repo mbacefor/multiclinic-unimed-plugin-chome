8adf0783cac49ff600e9a7b0e8e940e85f763e29
Marcelo <marcelo.alcantara@serpro.gov.br>
2021-09-19 00:17:06 -0300

    Versão com o nove ajuste: nova url e validacao

--- background.js
+++ background.js
@@ -5,11 +5,21 @@ function ajustaPagina() {
 
   document.body.style.backgroundColor = 'red';
 
-  const LINK_FORM = 'https://docs.google.com/forms/d/e/1FAIpQLSfza8do4fbcgDcInipY_Dbd-vBmUn6jjE76LRtYy-mRmH2zkw/viewform?usp=pp_url&entry.89253311=DATA_HORA&entry.1826533171=SERVICO&entry.646313095=PROFISSIONAL&entry.83580986=Unimed&entry.2096031278=BENEFICIARIO&entry.1179655987=NUM_PROCEDIMENTO&entry.166796424=100&entry.1463770423=NUM_SOLICITACAO';
+  //const LINK_FORM = 'https://docs.google.com/forms/d/e/1FAIpQLSfza8do4fbcgDcInipY_Dbd-vBmUn6jjE76LRtYy-mRmH2zkw/viewform?usp=pp_url&entry.89253311=DATA_HORA&entry.1826533171=SERVICO&entry.646313095=PROFISSIONAL&entry.83580986=Unimed&entry.2096031278=BENEFICIARIO&entry.1179655987=NUM_PROCEDIMENTO&entry.166796424=100&entry.1463770423=NUM_SOLICITACAO';
+
+  const LINK_FORM = 'https://docs.google.com/forms/d/e/1FAIpQLSdBUUyaruhKbdNJNcnOVqayBkvMWlp24w1PSYMGX13ZOQ4qKg/viewform?usp=pp_url&entry.89253311=DATA_HORA&entry.1826533171=SERVICO&entry.646313095=PROFISSIONAL&entry.83580986=Unimed&entry.2096031278=BENEFICIARIO&entry.1179655987=NUM_PROCEDIMENTO&entry.166796424=100&entry.1882838109=NUM_SOLICITACAO';
 
   const ID_TAB_UNIMED_PRODUCAO = 'linha';
 
-  const PROFISSIONAIS = ['Alexandra', 'Domingos Jr', 'Edinho', 'Jonas', 'Juliane', 'Karine (Fisi)', 'Paula Abreu', 'Paulo Rafael', 'Rayara', 'Raphaela', 'Rayana (Fono)', 'Thais de Sa'];
+  const PROFISSIONAIS = ['Alexandra', 'Domingos Jr', 'Edinho', 'Jonas', 'Juliane', 'Karine (Fisi)', 'Paula Abreu', 'Paulo Rafael', 'Rayara', 'Raphaela', 'Rayana (Fono)', 'Thaís de Sá'];
+
+  const NUMERO_SOLICITACAO = 6;
+
+  const COD_SERVICO = 7;
+
+  const NUM_BENEFICIARIO = 4;
+
+  const DATA_HORA = 0;
 
   /**
    * Gerar a TAG <A> com os paramentos para o formularios
@@ -17,15 +27,15 @@ function ajustaPagina() {
   function geraLinkFormulario(linha) {
 
     var a = document.createElement('A');
-    var linkText = document.createTextNode('Link formulário:' + linha.cells[6].innerHTML);
+    var linkText = document.createTextNode('Link formulário:' + linha.cells[NUMERO_SOLICITACAO].innerHTML);
     a.appendChild(linkText);
-    a.title = 'Link formulário:' + linha.cells[6].innerHTML;
-    var novoModeloData = linha.cells[0].innerHTML.substr(6, 4) + '-' + linha.cells[0].innerHTML.substr(3, 2) + '-' + linha.cells[0].innerHTML.substr(0, 2) + linha.cells[0].innerHTML.substr(10, 6);
+    a.title = 'Link formulário:' + linha.cells[NUMERO_SOLICITACAO].innerHTML;
+    var novoModeloData = linha.cells[DATA_HORA].innerHTML.substr(6, 4) + '-' + linha.cells[DATA_HORA].innerHTML.substr(3, 2) + '-' + linha.cells[DATA_HORA].innerHTML.substr(0, 2) + linha.cells[DATA_HORA].innerHTML.substr(10, 6);
 
     a.href = LINK_FORM.replace('DATA_HORA', novoModeloData);
     // Identifica o procedimento
     var servico = '';
-    switch (linha.cells[7].innerHTML) {
+    switch (linha.cells[COD_SERVICO].innerHTML) {
       case '5000008':
         servico = 'Terapia Ocupacional';
         break;
@@ -48,16 +58,16 @@ function ajustaPagina() {
         // servico = 'Fonoaudiologia';
         //break;
       default:
-        if (linha.cells[7].innerHTML.substr(0, 4) == '2010')
+        if (linha.cells[COD_SERVICO].innerHTML.substr(0, 4) == '2010')
           servico = 'Fisioterapia';
         else servico = 'Outros';
         break;
     }
     a.href = a.href.replace('SERVICO', servico);
     a.href = a.href.replace('PROFISSIONAL', document.getElementById('profissional').value);
-    a.href = a.href.replace('BENEFICIARIO', linha.cells[4].innerHTML);
-    a.href = a.href.replace('NUM_PROCEDIMENTO', linha.cells[7].innerHTML);
-    a.href = a.href.replace('NUM_SOLICITACAO', linha.cells[6].innerHTML);
+    a.href = a.href.replace('BENEFICIARIO', linha.cells[NUM_BENEFICIARIO].innerHTML);
+    a.href = a.href.replace('NUM_PROCEDIMENTO', linha.cells[COD_SERVICO].innerHTML);
+    a.href = a.href.replace('NUM_SOLICITACAO', linha.cells[NUMERO_SOLICITACAO].innerHTML);
     a.target = "_blank";
 
     return a;
@@ -148,9 +158,6 @@ function ajustaPagina() {
     } else alert('Esse plug-in somente executa no Relatório de Produção!');
 
   }
-
-
-
 
   //chama adicionar link
   adicionaLinkTabela();
